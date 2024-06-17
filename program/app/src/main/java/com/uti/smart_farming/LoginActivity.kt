d6a305519cee2cbab76a869ea19ed967f6978a3c
Rendy221104 <tprendy43@gmail.com>
2024-06-17 09:07:24 +0700

    buat variabel binding untuk LoginActivity

--- program/app/src/main/java/com/uti/smart_farming/LoginActivity.kt
+++ program/app/src/main/java/com/uti/smart_farming/LoginActivity.kt
@@ -1,14 +1,28 @@
 package com.uti.smart_farming
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uti.smart_farming.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
+//      buat variabel binding untuk LoginActivity
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
